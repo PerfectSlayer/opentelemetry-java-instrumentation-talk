26345a24ef0c12efdd6d72f08a939a8247a70e37
Bruce Bujon <bruce.bujon@datadoghq.com>
2023-05-07 18:25:45 +0200

    Add steps and code regions

--- storage/src/main/java/com/datadoghq/App.java
+++ storage/src/main/java/com/datadoghq/App.java
@@ -5,18 +5,14 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
@@ -33,8 +29,12 @@ public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) throws IOException {
-//        initOpenTelemetry();
-        autoconfigureOpenTelemetry();
+        //region step1
+        initOpenTelemetry();
+        //endregion
+        //region step2
+//        AutoConfiguredOpenTelemetrySdk.initialize();
+        //endregion
         // Create simple webserver
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/storage", new StorageHandler());
@@ -42,60 +42,41 @@ public class App {
         LOGGER.info("Server starts");
     }
 
-    static void initOpenTelemetryFull() {
-        Resource resource = Resource.getDefault().merge(
-                Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "storage"))
-        );
-
-        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(
-                        BatchSpanProcessor.builder(
-//                                OtlpHttpSpanExporter.builder().build()
-                                OtlpGrpcSpanExporter.builder()
-                                        .setEndpoint("http://datadog-agent-otel:4317")
-                                        .build()
-                        ).build())
-                .setResource(resource)
-                .build();
-
-        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-                .setTracerProvider(sdkTracerProvider)
-//                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-                .buildAndRegisterGlobal();
-    }
-
     static void initOpenTelemetry() {
+        //region Create resource
         Resource resource = Resource.getDefault().merge(
-                Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "storage"))
+                Resource.create(Attributes.of(
+                        ResourceAttributes.SERVICE_NAME, "storage",
+                        ResourceAttributes.DEPLOYMENT_ENVIRONMENT, "otel-demo"
+                ))
         );
+        //endregion
 
+        //region Create trace provider
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
                 .addSpanProcessor(
                         BatchSpanProcessor.builder(
                                 OtlpGrpcSpanExporter.builder()
                                         .setEndpoint("http://datadog-agent-otel:4317")
                                         .build()
-//                                OtlpHttpSpanExporter.builder()
-//                                        .setEndpoint("http://datadog-agent-otel:4318")
-//                                .build()
                         ).build())
                 .setResource(resource)
                 .build();
+        //endregion
 
-        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+        //region Register OpenTelemetry
+        OpenTelemetrySdk.builder()
                 .setTracerProvider(sdkTracerProvider)
                 .buildAndRegisterGlobal();
-    }
-
-    static void autoconfigureOpenTelemetry() {
-        AutoConfiguredOpenTelemetrySdk.initialize();
-//        OpenTelemetrySdk sdk = AutoConfiguredOpenTelemetrySdk.initialize()
-//                .getOpenTelemetrySdk();
+        //endregion
     }
 
     static class StorageHandler implements HttpHandler {
-
+        //region step1
         private final Tracer tracer = GlobalOpenTelemetry.getTracer("http-handler");
+        //endregion
+
+        //region step3
         private final TextMapGetter<Headers> getter = new TextMapGetter<>() {
             @Override
             public Iterable<String> keys(Headers headers) {
@@ -107,40 +88,50 @@ public class App {
                 return headers.getFirst(key);
             }
         };
+        //endregion
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            Context context = extractContext(exchange);
-            Span span = tracer.spanBuilder("handleRequest")
-                    .setParent(context)
-                    .setSpanKind(SpanKind.SERVER)
-                    .startSpan();
-            try (Scope scope = span.makeCurrent()) {
-                LOGGER.info("Handling request");
-                // Compute storage answer
-                String item = extractItem(exchange);
-                int count = getCount(item);
-                String response = "{\"name\": \""+item+"\", \"count\": " + count + "}";
-                // Answer to client
-                var headers = exchange.getResponseHeaders();
-                headers.add("Content-Type", "application/json; charset=utf-8");
-                exchange.sendResponseHeaders(200, response.length());
-                try (OutputStream outputStream = exchange.getResponseBody()) {
-                    outputStream.write(response.getBytes(UTF_8));
-                    outputStream.flush();
-                }
-            } finally {
-                span.end();
+            //region step3
+//            Context context = extractContext(exchange);
+//                    .setParent(context)
+            //endregion
+
+            //region step1
+//            Span span = tracer.spanBuilder("handleRequest")
+//                    .setSpanKind(SpanKind.SERVER)
+//                    .startSpan();
+//            try (Scope scope = span.makeCurrent()) {
+            //endregion
+            LOGGER.info("Handling request");
+            // Compute storage answer
+            String item = extractItem(exchange);
+            int count = getCount(item);
+            String response = "{\"name\": \"" + item + "\", \"count\": " + count + "}";
+            // Answer to client
+            var headers = exchange.getResponseHeaders();
+            headers.add("Content-Type", "application/json; charset=utf-8");
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream outputStream = exchange.getResponseBody()) {
+                outputStream.write(response.getBytes(UTF_8));
+                outputStream.flush();
             }
+            //region step1
+//            } finally {
+//                span.end();
+//            }
+            //endregion
         }
 
-        private Context extractContext(HttpExchange exchange) {
-            return W3CTraceContextPropagator.getInstance().extract(
-                    Context.root(),
-                    exchange.getRequestHeaders(),
-                    getter
-            );
-        }
+        //region step3
+//        private Context extractContext(HttpExchange exchange) {
+//            return W3CTraceContextPropagator.getInstance().extract(
+//                    Context.root(),
+//                    exchange.getRequestHeaders(),
+//                    getter
+//            );
+//        }
+        //endregion
 
         private String extractItem(HttpExchange exchange) {
             String path = exchange.getRequestURI().getPath();
@@ -148,21 +139,25 @@ public class App {
             if (index == -1) {
                 return path;
             } else {
-                return path.substring(index+1);
+                return path.substring(index + 1);
             }
         }
 
         private int getCount(String item) {
+            //region step1
             Span span = tracer.spanBuilder("computeCount")
                     .setAttribute("item", item)
                     .startSpan();
+            //endregion
             // Simulate computation
             try {
                 Thread.sleep((long) (Math.random() * 100 + 150));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+            //region step1
             span.end();
+            //endregion
             return (int) (Math.random() * 150);
         }
     }
